21b41f0cc8bb06e3152ebcf918250386c7f8aa2d
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2019-08-08 22:40:23 +0900

    Refactor

--- abc083/B/main.py
+++ abc083/B/main.py
@@ -2,12 +2,7 @@ n, a, b = map(int, input().split())
 
 l = []
 for i in range(1, n + 1):
-    if len(str(i)) == 1:
-        if i >= a and i <= b:
-            l.append(i)
-    else:
-        li = [int(str(i)[j]) for j in range(len(str(i)))]
-        if sum(li) >= a and sum(li) <= b:
-            l.append(i)
+    if a <= sum(list(map(int, list(str(i))))) <= b:
+        l.append(i)
 
 print(sum(l))
