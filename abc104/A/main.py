5e9b7cefaca97fd3b0bf7b510cd937ae9da281c8
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2019-09-02 19:29:01 +0900

    refactor

--- abc104/A/main.py
+++ abc104/A/main.py
@@ -1,8 +1,2 @@
 R = int(input())
-
-if R < 1200:
-    print('ABC')
-elif 1200 <= R < 2800:
-    print('ARC')
-else:
-    print('AGC')
+print('ABC' if R < 1200 else 'ARC' if 1200 <= R < 2800 else 'AGC')
